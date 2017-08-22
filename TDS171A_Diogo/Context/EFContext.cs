b3dfae9956332af1d3b3b0f97eccb7cde7c682d6
Master <Lab. Desenvolvimento@RB-LAB102-10>
2017-08-22 20:25:02 -0300

    suppliers

--- TDS171A_Diogo/Context/EFContext.cs
+++ TDS171A_Diogo/Context/EFContext.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Data.Entity;
 using System.Web;
+using TDS171A_Diogo.Models;
 
 namespace TDS171A_Diogo.Context
 {
     public class EFContext : DbContext
     {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Supplier> Suppliers { get; set; }
+
         public EFContext()
             : base ("Asp_Net_MVC_CS")
         {
---
+++ TDS171A_Diogo/Controllers/SuppliersController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TDS171A_Diogo.Context;
+
+namespace TDS171A_Diogo.Controllers
+{
+    public class SuppliersController : Controller
+    {
+        private readonly EFContext _context = new EFContext();
+        // GET: Suppliers
+        public ActionResult Index()
+        {
+            return View(_context
+                .Suppliers
+                .OrderBy( s => s.Name));
+        }
+    }
+}
