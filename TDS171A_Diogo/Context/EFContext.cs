90832e2c9a2e7cb9d9d4fe5869d27710eb5a0e1b
Master <Lab. Desenvolvimento@RB-LAB102-10>
2017-08-22 20:00:13 -0300

    Primeiras alterações + Database

---
+++ TDS171A_Diogo/Context/EFContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.Entity;
+using System.Web;
+
+namespace TDS171A_Diogo.Context
+{
+    public class EFContext : DbContext
+    {
+        public EFContext()
+            : base ("Asp_Net_MVC_CS")
+        {
+                
+        }
+    }
+}
---
+++ TDS171A_Diogo/Models/Supplier.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TDS171A_Diogo.Models
+{
+    public class Supplier
+    {
+        public long SupplierId { get; set; }
+
+        public string Name { get; set; }
+    }
+}
