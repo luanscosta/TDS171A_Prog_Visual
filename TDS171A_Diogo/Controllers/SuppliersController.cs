03cdb4b351b7dae3e3e4b37241328a6f0d97b3d8
Master <Lab. Desenvolvimento@RB-LAB102-10>
2017-08-22 21:51:58 -0300

    Suppliers com o edit

--- TDS171A_Diogo/Controllers/SuppliersController.cs
+++ TDS171A_Diogo/Controllers/SuppliersController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using TDS171A_Diogo.Context;
@@ -16,7 +18,7 @@ namespace TDS171A_Diogo.Controllers
         {
             return View(_context
                 .Suppliers
-                .OrderBy( s => s.Name));
+                .OrderBy(s => s.Name));
         }
         public ActionResult Create(Supplier supplier)
         {
@@ -24,14 +26,47 @@ namespace TDS171A_Diogo.Controllers
             _context.SaveChanges();
 
             return RedirectToAction("Index");
-            
+
         }
         [HttpPost]
         [ValidateAntiForgeryToken]
 
-        public ActionResult Edit()
+        public ActionResult Edit(long? id)
         {
-            return View();
+            if (!id.HasValue)
+            {
+                return new HttpStatusCodeResult(
+                    HttpStatusCode.BadRequest);
+            }
+            var supplier = _context
+                .Suppliers
+                .Find(id.Value);
+            if (supplier == null)
+            {
+                return new HttpStatusCodeResult(
+                    HttpStatusCode.NotFound);
+            }
+
+            return View(supplier);
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public ActionResult Edit(Supplier supplier)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Entry(supplier)
+                   .State = EntityState.Modified;
+                _context.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View(supplier);
+        }
+
+
     }
 }
